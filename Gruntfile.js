660555186250994f952c684002e5b506f28989f5
Jeff Herb <herb.jeff@example.com>
2018-09-03 07:58:57 -0400

    added some more base stlyes and fixed grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,45 +4,76 @@ module.exports = grunt => {
   // Load all grunt tasks matching the ['grunt-*', '@*/grunt-*'] patterns
   require('load-grunt-tasks')(grunt);
 
-  grunt.initConfig({
+  grunt.initConfig(
 
-    connect: {
-      server: {
-        options: {
-          port: 8000,
-          base: {
-            path: 'dist',
-            // options: {
-            //   //index: 'somedoc.html',
-            //   //maxAge: 300000
-            // }
+    {
+      clean: ['dist'],
+
+      connect: {
+        server: {
+          options: {
+            port: 8000,
+            base: {
+              path: 'dist',
+            }
           }
         }
-      }
-    },
-
-    sass: {
-      options: {
-        implementation: sass,
-        sourceMap: true
       },
-      dist: {
-        files: {
-          'dist/main.css': 'src/scss/main.scss'
-        }
-      }
-    },
 
-    watch: {
-      readme: {
-        files: ['README.md'],
-        tasks: []
+      copy: {
+        html: {
+          expand: true,
+          cwd: 'src/html/',
+          src: '**/*.html',
+          dest: 'dist/',
+          flatten: true,
+          filter: 'isFile',
+        },
+        images: {
+          expand: true,
+          cwd: 'src/images/',
+          src: ['**/*.jpg', '**/*.png', '**/*.gif'],
+          dest: 'dist/images/',
+          flatten: true,
+          filter: 'isFile',
+        }
+      },
+
+      sass: {
+        options: {
+          implementation: sass,
+          sourceMap: true
+        },
+        dist: {
+          files: {
+            'dist/css/main.css': 'src/scss/main.scss'
+          }
+        }
+      },
+
+      watch: {
+        css: {
+          files: 'src/scss/**/*.scss',
+          tasks: ['sass']
+        },
+        images: {
+          files: ['src/images/**/*.jpg','src/images/**/*.png','src/images/**/*.gif'],
+          tasks: ['copy:images']
+        },
+        html: {
+          files: 'src/html/**/*.html',
+          tasks: ['copy:html']
+        },
+        readme: {
+          files: ['README.md'],
+          tasks: []
+        }
       }
     }
 
-  });
+  );
 
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['clean', 'copy', 'sass']);
 
-  grunt.registerTask('dev', ['sass', 'connect', 'watch']);
+  grunt.registerTask('dev', ['clean', 'copy', 'sass', 'connect', 'watch']);
 };
